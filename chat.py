01ec6edc92895df8b0ed8a193e26b9b482271567
renan <renanbazinin2@gmail.com>
2023-05-30 19:51:42 +0300

    roll back to me

--- chat.py
+++ chat.py
@@ -1,4 +1,5 @@
 import time
+import subprocess
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -13,6 +14,7 @@ chrome_options.add_argument("--headless")
 # Path to your ChromeDriver executable
 chromedriver_path = '/path/to/chromedriver'
 
+lastResult ="empty"
 while True:
     # Set up the ChromeDriver service
     service = Service(chromedriver_path)
@@ -36,13 +38,20 @@ while True:
     # Find the first network request that matches the URL prefix
     target_url = 'https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12dvr/index.m3u8?b-in-range=100-2700'
     filtered_request = next((entry['name'] for entry in network_entries if entry['name'].startswith(target_url)), None)
-
+    if(lastResult == filtered_request):
+        print("no change")
     if filtered_request:
         # Create the text file and write the content
-        with open('ke12.m3u', 'w') as file:
+        with open('playlist.m3u', 'w') as file:
             file.write('#EXTM3U\n')
             file.write('#EXTINF:-1 tvg-id="Keshet 12 IL" tvg-name="Keshet 12 IL" tvg-logo="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Keshet12_2018.svg/1200px-Keshet12_2018.svg.png" group-title="Israel",keshet12\n')
             file.write(filtered_request)
+
+        # Add, commit, and push changes using Git
+        subprocess.run(['git', 'add', 'playlist.m3u'])
+        subprocess.run(['git', 'commit', '-m', 'Updated playlist'])
+        subprocess.run(['git', 'push'])
+
     else:
         print("No matching network request found.")
 
@@ -50,4 +59,4 @@ while True:
     driver.quit()
 
     # Delay for 3 minutes before the next execution
-    time.sleep(180)
+    time.sleep(222)
